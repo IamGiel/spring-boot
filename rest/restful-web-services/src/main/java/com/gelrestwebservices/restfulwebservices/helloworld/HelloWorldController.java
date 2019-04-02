3ca92f50cb43402cf9c0e5b2570fabd252fa888b
IamGiel <november14giel@gmail.com>
2019-04-02 10:42:04 -0400

    adding i18n

--- rest/restful-web-services/src/main/java/com/gelrestwebservices/restfulwebservices/helloworld/HelloWorldController.java
+++ rest/restful-web-services/src/main/java/com/gelrestwebservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.gelrestwebservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	//create method to return hello world
 	//GET
 	//URI /hello-world
@@ -26,4 +35,15 @@ public class HelloWorldController {
 	public	HelloWorldBean HelloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello PAth Variable %s", name));
 	}
+	
+	///hello-world/international
+	@GetMapping(path="/hello-world/international")
+	public	String helloWorldInternational(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		return messageSource.getMessage("good.morning.message",null, locale);
+	}
+	
 }
+
+
+
+
